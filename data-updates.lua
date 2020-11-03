91f58de0fd2a562abb4bacbb7cb8a69746e6fd95
AlexandrVolkov <alexandr25011987@gmail.com>
2020-11-03 16:20:46 +0200

    Update data-updates.lua

--- data-updates.lua
+++ data-updates.lua
@@ -1,4 +1,3 @@
--- TODO: change dying_explosion and corpse. Add customizable mod settings.
 
 local entity, name
 
@@ -12,10 +11,8 @@ end
 name = "locomotive"
 entity = data.raw[name][name]
 if entity then
-	multiply_property("weight", 3)
-	multiply_property("max_health", 1.2)
-	multiply_property("max_speed", 0.5)
-	entity.max_power = "1200kW" -- TODO: change
+	multiply_property("max_speed", 1.5)
+	entity.max_power = "4000kW" -- TODO: change
 	if entity.burner.effectivity ~= 0 then
 		entity.burner.effectivity = entity.burner.effectivity * 2
 	end
@@ -25,8 +22,6 @@ end
 name = "cargo-wagon"
 entity = data.raw[name][name]
 if entity then
-	multiply_property("weight", 3)
-	multiply_property("max_health", 1.2)
 	multiply_property("friction_force", 5)
 	multiply_property("inventory_size", 1.5)
 end
@@ -34,34 +29,7 @@ end
 name = "fluid-wagon"
 entity = data.raw[name][name]
 if entity then
-	multiply_property("weight", 3)
-	multiply_property("max_health", 1.2)
 	multiply_property("friction_force", 5)
 	multiply_property("capacity", 1.5)
 end
 
-
--- For https://mods.factorio.com/mod/OS-Speed-Train
----------------------------------------------------
-name = "speed-train"
-entity = data.raw["locomotive"][name]
-if entity then
-	multiply_property("weight", 3)
-	multiply_property("max_health", 1.2)
-	multiply_property("max_speed", 0.5)
-	entity.max_power = "4000kW" -- TODO: change
-	if entity.burner.effectivity ~= 0 then
-		entity.burner.effectivity = entity.burner.effectivity * 2
-	end
-	multiply_property("friction_force", 8)
-end
-
-name = "speed-cargo-wagon"
-entity = data.raw["cargo-wagon"][name]
-if entity then
-	multiply_property("weight", 3)
-	multiply_property("max_health", 1.2)
-	multiply_property("friction_force", 5)
-	multiply_property("inventory_size", 1.5)
-end
----------------------------------------------------
